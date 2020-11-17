9f9dd8b717b4db6d386f00c553bc911c19610b68
l <lyonlg@hendrix.edu>
2020-11-17 12:25:50 -0600

    formatted quote page - button doesnt work

--- lib/QuotePage.dart
+++ lib/QuotePage.dart
@@ -16,13 +16,36 @@ class QuotePageState extends State<QuotePage> {
 
   @override
   Widget build(BuildContext context) {
+    String quote = _pickQuote();
     return Scaffold(
+      backgroundColor: Colors.pink[100],
       appBar: AppBar(
         title: const Text('Inspirational Quotes'),
       ),
       body: Center(
-        child: Text(_pickQuote()),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget> [
+          Text( quote ,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 48.0,
+            fontWeight: FontWeight.bold,
+            //fontFamily: Font
+          )
+        ),
+       // Text('button here')
+      RaisedButton(
+          onPressed: () {
+            quote = _pickQuote();
+          },
+          child: Text("this doesnt work"), //goal is to change to new quote
+      )
+      ]
       ),
+      )
     );
   }
 }
