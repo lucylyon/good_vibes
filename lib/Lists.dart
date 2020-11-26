5d8963b81a3d52d897af38ecbbbf6e281a153e87
l <lyonlg@hendrix.edu>
2020-11-25 21:06:22 -0600

    pictures work

--- lib/Lists.dart
+++ lib/Lists.dart
@@ -19,3 +19,35 @@ List<String> quotes = [
 List<Friend> friendList = [
   Friend("127.0.0.1", "Self")
 ];
+
+List<String> pictures = [
+  'assets/cat.jpg',
+  'assets/beef.jpg',
+  'assets/chicken.png',
+  'assets/cow.jpg',
+  'assets/cowch.jpg',
+  'assets/cute.jpg',
+  'assets/dogs.jpg',
+  'assets/duck.jpg',
+  'assets/flat.jpg',
+  'assets/frog.jpg',
+  'assets/frog2.jpg',
+  'assets/gecko.jpg',
+  'assets/mail.jpg',
+  'assets/pig.jpg',
+  'assets/quote.jpg',
+  'assets/rat2.jpg',
+  'assets/rat3.jpg',
+  'assets/spooky.jpg',
+  'assets/spooky2.jpg',
+  'assets/tot.jpg',
+  'assets/worms.jpg',
+  'assets/corg.jpg',
+  'assets/love.jpg',
+  'assets/pup.jpg',
+  'assets/flower.jpg',
+  'assets/sky.jpg',
+  'assets/plant.jpg',
+  'assets/shoe.jpg',
+  'assets/friend.jpg'
+];
--- lib/PicturePage.dart
+++ lib/PicturePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'Lists.dart';
+
 
 class PicturePage extends StatefulWidget{
 
@@ -7,8 +9,17 @@ class PicturePage extends StatefulWidget{
 }
 
 class PicturePageState extends State<PicturePage> {
+
+  String picture;
+
+  String _pickPicture(){
+    pictures.shuffle();
+    return pictures.last;
+  }
+
   @override
   Widget build(BuildContext context) {
+    picture =  _pickPicture();
     return Scaffold(
       backgroundColor: Colors.cyan[100],
       appBar: AppBar(
@@ -16,8 +27,26 @@ class PicturePageState extends State<PicturePage> {
         backgroundColor: Colors.cyanAccent[700],
       ),
       body: Center(
-        child: Text("Pictures go here"),
-      ),
-    );
+    child: new Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget> [
+    Padding(
+      padding: EdgeInsets.fromLTRB(0,0,0,50),
+        child: Image(image: AssetImage(picture))
+    ),
+    RaisedButton(
+      padding: EdgeInsets.all(20),
+      color: Colors.cyanAccent[700],
+        onPressed: () {
+          setState(() => picture = _pickPicture());
+      },
+      child: Text("Get another picture",
+        style: TextStyle(
+        color: Colors.white,
+        fontSize: 18),
+      ))
+    ]
+    ),));
   }
 }
